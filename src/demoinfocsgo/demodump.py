0e987a5ec3a23cc3a2bd59197579304478bf2f9e
Tony Au-Yeung <tonyay163@gmail.com>
2016-03-17 13:19:09 +0800

    Fixed up the example so that it actually works Changed event number to event name (apparently the number can change) Removed index because it wasn't really useful Fixed bugs where the name is used but the name isn't really their name Fixed a bug with networkid because it's not passed in event single connect Removed some useless statements that are half missing Fixed a bug in demodump where user message type might not exist (Need to figure out why that happens)

--- src/demoinfocsgo/demodump.py
+++ src/demoinfocsgo/demodump.py
@@ -16,12 +16,12 @@ _GAMEEVENT_TYPES = {2:"val_string",
                    7:"val_bool",
                    8:"val_uint64",
                    9:"val_wstring"}
- 
+
 class GameEvent(object):
     '''
     Object that is passed when a game event is fired, for all game events, see: data/game_events.txt
     '''
-    
+
     def __init__(self, raw, descriptor, name):
         self.raw = raw
         self.descriptor = descriptor
@@ -31,7 +31,7 @@ class GameEvent(object):
         for keyname in self.descriptor[3]:
             setattr(self, keyname[1], getattr(self.raw.keys[index], _GAMEEVENT_TYPES[keyname[0] + 1]))
             index += 1
-       
+
 class DemoDump(object):
     '''
     Dumps a CSGO demo
@@ -84,15 +84,15 @@ class DemoDump(object):
         self.register_on_netmsg(svc_UpdateStringTable, self._update_stringtable)
         self.register_on_netmsg(svc_ClassInfo, self._handle_classinfo)
         self.register_on_netmsg(net_Tick, self._handle_tick)
-        
+
     def open(self, filename):
         '''
         Opens the demo file.
         '''
         self.demofile = DemoFile()
         return self.demofile.open(filename)
-    
-    
+
+
     def register_on_netmsg(self, msg, callback):
         '''
         Will register a callback to call when the given netmessage is received.
@@ -101,7 +101,7 @@ class DemoDump(object):
         if msg not in self.NET_MSG:
             raise "Net message not found"
         self.NET_MSG[msg].append(callback)
-    
+
     def register_on_gameevent(self, msg, callback):
         '''
         Will register a callback to call when the given game event is called.
@@ -111,7 +111,7 @@ class DemoDump(object):
         if msg not in self.GAME_EVENTS:
             self.GAME_EVENTS[msg] = []
         self.GAME_EVENTS[msg].append(callback)
-    
+
     def register_on_usermessage(self, msg, callback):
         '''
         Not yet implemented.
@@ -119,7 +119,7 @@ class DemoDump(object):
         if not msg in self.USER_MESSAGES:
             self.USER_MESSAGES[msg] = []
         self.USER_MESSAGES[msg].append(callback)
-    
+
     def dump(self):
         '''
         Will start analyzing the demo and executing the necessary game events
@@ -137,17 +137,18 @@ class DemoDump(object):
                 self.demofile.read_raw_data()
             elif cmd == DemoMessage.DATATABLES:
                 self.demofile.read_raw_data()
-
             elif cmd == DemoMessage.STRINGTABLES:
                 self.demofile.read_raw_data()
             elif cmd == DemoMessage.USERCMD:
                 self.demofile.read_user_cmd()
             elif cmd == DemoMessage.SIGNON or cmd == DemoMessage.PACKET:
                 self._handle_demo_packet()
-    
+
     def _handle_usermessage(self, cmd, data):
         umsg = CSVCMsg_UserMessage()
         umsg.ParseFromString(data)
+        if umsg.msg_type not in ECstrike15UserMessages.DESCRIPTOR.values_by_number:
+            return # TODO: Find out what to do with this
         name = ECstrike15UserMessages.Name(umsg.msg_type)
         callback_name = name.replace("CS_UM_", "")
         if callback_name in self.USER_MESSAGES:
@@ -156,17 +157,17 @@ class DemoDump(object):
             item.ParseFromString(umsg.msg_data)
             for callback in self.USER_MESSAGES[callback_name]:
                 callback(item)
-                
+
     def _handle_tick(self, cmd, data):
         tickinfo = CNETMsg_Tick()
         tickinfo.ParseFromString(data)
         self.current_tick = tickinfo.tick
-    
+
     def _server_info_update(self, cmd, data):
         info = CSVCMsg_ServerInfo()
         info.ParseFromString(data)
         self.server_info = info
-    
+
     def _handle_gameeventlist(self, cmd, data):
         '''
         Parses the list of given game events by the server
@@ -177,7 +178,7 @@ class DemoDump(object):
             self.descriptors[desc.eventid] = (desc.eventid, desc.name, desc.keys, [])
             for key in desc.keys:
                 self.descriptors[desc.eventid][3].append([key.type, key.name])
-    
+
     def _handle_gameevent(self, cmd, data):
         '''
         handles the game events and fires the callback
@@ -189,13 +190,13 @@ class DemoDump(object):
             event = GameEvent(gameevent, self.descriptors[gameevent.eventid], name)
             for callback in self.GAME_EVENTS[event.raw.eventid]:
                 callback(event)
-                
-        
+
+
         if name in self.GAME_EVENTS:
             event = GameEvent(gameevent, self.descriptors[gameevent.eventid], name)
             for callback in self.GAME_EVENTS[name]:
                 callback(event)
-         
+
     stringtable_data = {}
     def _server_create_stringtable(self, cmd, data):
         '''
@@ -222,7 +223,7 @@ class DemoDump(object):
         pRestore->ReadString( pTemp, nLen, nLen );
         *pStringIndex = m_pStringTable->AddString( CBaseEntity::IsServer(), pTemp );
         '''
-        
+
     def _update_stringtable(self, cmd, data):
         '''
         Updates a stringtable with the given data
@@ -230,7 +231,7 @@ class DemoDump(object):
         '''
         update = CSVCMsg_UpdateStringTable()
         update.ParseFromString(data)
-        
+
     def _send_table(self, cmd, data):
         table = CSVCMsg_SendTable()
         table.ParseFromString(data)
@@ -238,23 +239,23 @@ class DemoDump(object):
         for prop in table.props:
             print "Name: %s type: %i" % (prop.var_name, prop.type)
         print "-------------------------------"
-        
+
     def _handle_classinfo(self, cmd, data):
         info = CSVCMsg_ClassInfo()
         info.ParseFromString(data)
         #print "CLASS INFO COUNT: %s, CREATE ON CLIENT: %r" % (len(info.classes), info.create_on_client)
         #for c in info.classes:
         #    print "%i: %s, %s" % (c.class_id, c.data_table_name, c.class_name)
-                
+
     def _handle_demo_packet(self):
         info = self.demofile.read_cmd_info()
         self.demofile.read_sequence_info()  # ignore result
         length, buf = self.demofile.read_raw_data()
-        
+
         if length > 0:
             self._dump_packet(buf, length)
-         
-            
+
+
     def _dump_packet(self, buf, length):
         index = 0
         while index < length:
@@ -263,11 +264,11 @@ class DemoDump(object):
             data = buf[index:index + size]
             if cmd in self.NET_MSG:
                 for callback in self.NET_MSG[cmd]:
-                    
+
                     callback(cmd, data);
-                    
+
             index = index + size
-        
+
     def __read_int32(self, buf, index):
         '''
         Reads an int32 from a binary buffer.
@@ -275,7 +276,7 @@ class DemoDump(object):
         b = 0
         count = 0
         result = 0
-        
+
         cont = True
         while cont:
             if count == 5:
@@ -287,4 +288,3 @@ class DemoDump(object):
             count = count + 1
             cont = b & 0x80
         return result, index
-            
